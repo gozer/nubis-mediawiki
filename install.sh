b44ba3ebf8d31c4f3094afe6c6f27b9cdd90db29
uberj <jaxbr503@gmail.com>
2014-10-22 15:13:17 -0700

    update install script

--- install.sh
+++ install.sh
@@ -3,15 +3,17 @@
 grep '. /etc/environment' /etc/apache2/envvars
 
 if [ $? -eq 1 ]; then
-        echo '. /etc/environment' >> /etc/apache2/envvars
+    echo '. /etc/environment' >> /etc/apache2/envvars
 fi
 
-git clone https://github.com/wikimedia/mediawiki.git
-pushd mediawiki
-    ln -s ../LocalSettings.php LocalSettings.php
-    # install plugins and whatever
-    git checkout 1.23.5
-popd
-
-service apache2 restart
+cd $(dirname $0)
+if [ ! -d mediawiki ]; then
+    git clone https://github.com/wikimedia/mediawiki.git
+    pushd mediawiki
+        ln -s ../LocalSettings.php LocalSettings.php
+        # install plugins and whatever
+        git checkout 1.23.5
+    popd
+    service apache2 restart
+fi
 
