c417baec4e6657b2e7801249d37f9f31cb408b97
uberj <jaxbr503@gmail.com>
2014-10-22 11:18:13 -0700

    tweaks

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+grep '. /etc/environment' /etc/apache2/envvars
+
+if [ $? -eq 1 ]; then
+        echo '. /etc/environment' >> /etc/apache2/envvars
+fi
+
 git clone https://github.com/wikimedia/mediawiki.git
 pushd mediawiki
     ln -s ../LocalSettings.php LocalSettings.php
     # install plugins and whatever
     git checkout 1.23.5
 popd
+
+service apache2 restart
+
