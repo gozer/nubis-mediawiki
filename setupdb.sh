0cd323050a4dffe579486db121e6f821eeb0bbc3
uberj <jaxbr503@gmail.com>
2014-10-23 14:53:36 -0700

    add setupdb.sh

---
+++ setupdb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+set -e
+
+if [[ -z $1 ]] || [[ -z $2 ]] || [[ -z $3 ]]; then
+    echo "USAGE: setupdb.sh <sql-dump-file> <user-name> <webserver-ip>"
+    exit 1
+fi
+db_file=$(basename $1)
+SSH_OPTIONS="-o StrictHostKeyChecking=no"
+scp $SSH_OPTIONS $1 $2@$3:/tmp/
+ssh $SSH_OPTIONS $2@$3 'bash -c "source /etc/environment && \
+    mysql -h$PROVISION_app_db_server --user=$PROVISION_db_username --password=$PROVISION_db_password --database=$PROVISION_db_name < /tmp/'$db_file'"'
